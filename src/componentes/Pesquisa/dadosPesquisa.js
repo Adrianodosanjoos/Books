a7414a7a428326c7573bfe4053b35de3ac35aa2d
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-08-20 14:30:15 -0300

    Atualização

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,13 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [ 
+    {nome: 'Liderança em Desing', id: 1, src:livro},
+    {nome: 'Para sempre ao seu lado', id: 2, src:livro},
+    {nome: 'Cangaceiro em JavaScript', id: 3, src:livro},
+    {nome: 'JavaScript Acertivo', id: 4, src:livro},
+    {nome: 'Nada é por Acaso', id: 5, src:livro},
+    {nome: '50 Tons de cinzas', id: 6, src:livro},
+    {nome: 'Aventureiros do Java', id: 7, src:livro},
+    {nome: '50 codigos de Java', id: 8, src:livro},
+    {nome: 'Codigos para iniciantes', id: 9, src:livro},
+]
